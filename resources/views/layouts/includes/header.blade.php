3e539879edd8900d7f5fbdb7c29fc50b1e086d75
dangthuaan <thuaan95@gmail.com>
2019-10-13 11:35:09 +0700

    1:Change the layout

---
+++ resources/views/layouts/includes/header.blade.php
@@ -0,0 +1,31 @@
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Koparion – Book Shop Bootstrap 4 Template</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/x-icon" href="/theme/img/favicon.png">
+    
+    <!-- all css here -->
+    <!-- bootstrap v3.3.6 css -->
+    <link rel="stylesheet" href="/theme/css/bootstrap.min.css">
+    <!-- animate css -->
+    <link rel="stylesheet" href="/theme/css/animate.css">
+    <!-- meanmenu css -->
+    <link rel="stylesheet" href="/theme/css/meanmenu.min.css">
+    <!-- owl.carousel css -->
+    <link rel="stylesheet" href="/theme/css/owl.carousel.css">
+    <!-- font-awesome css -->
+    <link rel="stylesheet" href="/theme/css/font-awesome.min.css">
+    <!-- flexslider.css-->
+    <link rel="stylesheet" href="/theme/css/flexslider.css">
+    <!-- chosen.min.css-->
+    <link rel="stylesheet" href="/theme/css/chosen.min.css">
+    <!-- style css -->
+    <link rel="stylesheet" href="/theme/style.css">
+    <!-- responsive css -->
+    <link rel="stylesheet" href="/theme/css/responsive.css">
+    <!-- modernizr css -->
+    <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+</head>
---
+++ resources/views/layouts/includes/script.blade.php
@@ -0,0 +1,27 @@
+<!-- all js here -->
+<!-- jquery latest version -->
+<script src="/theme/js/vendor/jquery-1.12.0.min.js"></script>
+<!-- bootstrap js -->
+<script src="/theme/js/bootstrap.min.js"></script>
+<!-- owl.carousel js -->
+<script src="/theme/js/owl.carousel.min.js"></script>
+<!-- meanmenu js -->
+<script src="/theme/js/jquery.meanmenu.js"></script>
+<!-- wow js -->
+<script src="/theme/js/wow.min.js"></script>
+<!-- jquery.parallax-1.1.3.js -->
+<script src="/theme/js/jquery.parallax-1.1.3.js"></script>
+<!-- jquery.countdown.min.js -->
+<script src="/theme/js/jquery.countdown.min.js"></script>
+<!-- jquery.flexslider.js -->
+<script src="/theme/js/jquery.flexslider.js"></script>
+<!-- chosen.jquery.min.js -->
+<script src="/theme/js/chosen.jquery.min.js"></script>
+<!-- jquery.counterup.min.js -->
+<script src="/theme/js/jquery.counterup.min.js"></script>
+<!-- waypoints.min.js -->
+<script src="/theme/js/waypoints.min.js"></script>
+<!-- plugins js -->
+<script src="/theme/js/plugins.js"></script>
+<!-- main js -->
+<script src="/theme/js/main.js"></script>
